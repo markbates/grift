f323c1aa4ec50bd856c80430c03c28de71aa8e0c
Mark Bates <mark@markbates.com>
2017-04-13 13:39:51 -0400

    Context now implements context.Context

--- grift/context.go
+++ grift/context.go
@@ -1,15 +1,37 @@
 package grift
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"path/filepath"
+	"runtime"
+)
+
 // Context used to pass information between grifts
 type Context struct {
+	context.Context
 	Name    string
 	Args    []string
 	Verbose bool
-	data    map[string]interface{}
+	data    map[interface{}]interface{}
 }
 
 // Get a piece of data from the Context.
 func (c *Context) Get(key string) interface{} {
+	warningMsg := "Context#Get is deprecated. Please use Context#Value instead."
+
+	_, file, no, ok := runtime.Caller(1)
+	if ok {
+		file = filepath.Base(file)
+		warningMsg = fmt.Sprintf("Context#Get is deprecated. Please use Context#Value instead. Called from grifts/%s:%d", file, no)
+	}
+	log.Println(warningMsg)
+
+	return c.data[key]
+}
+
+func (c *Context) Value(key interface{}) interface{} {
 	return c.data[key]
 }
 
@@ -21,8 +43,9 @@ func (c *Context) Set(key string, val interface{}) {
 // NewContext builds and returns a new default Context.
 func NewContext(name string) *Context {
 	return &Context{
-		Name: name,
-		Args: []string{},
-		data: map[string]interface{}{},
+		Context: context.Background(),
+		Name:    name,
+		Args:    []string{},
+		data:    map[interface{}]interface{}{},
 	}
 }
