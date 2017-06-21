2c35345f386946785d321ce7586b5f55f4e79480
Mark Bates <mark@markbates.com>
2017-06-21 13:17:26 -0400

    updated the init template to use the new Namespace function

--- cmd/templates.go
+++ cmd/templates.go
@@ -27,25 +27,27 @@ import (
 	. "github.com/markbates/grift/grift"
 )
 
+var _ = Desc("hello", "Say Hello!")
 var _ = Add("hello", func(c *Context) error {
 	fmt.Println("Hello World!")
 	return nil
 })
 
-
-var _ = Desc("env:print", "Prints out all of the ENV variables in your environment. Pass in the name of a particular ENV variable to print just that one out. (e.g. grift env:print GOPATH)")
-var _ = Add("env:print", func(c *Context) error {
-	if len(c.Args) >= 1 {
-		for _, e := range c.Args {
-			fmt.Printf("%s=%s\n", e, os.Getenv(e))
+var _ = Namespace("env", func() {
+	Desc("print", "Prints out all of the ENV variables in your environment. Pass in the name of a particular ENV variable to print just that one out. (e.g. grift env:print GOPATH)")
+	Add("print", func(c *Context) error {
+		if len(c.Args) >= 1 {
+			for _, e := range c.Args {
+				fmt.Printf("%s=%s\n", e, os.Getenv(e))
+			}
+		} else {
+			for _, e := range os.Environ() {
+				pair := strings.Split(e, "=")
+				fmt.Printf("%s=%s\n", pair[0], os.Getenv(pair[0]))
+			}
 		}
-	} else {
-		for _, e := range os.Environ() {
-			pair := strings.Split(e, "=")
-			fmt.Printf("%s=%s\n", pair[0], os.Getenv(pair[0]))
-		}
-	}
 
-	return nil
+		return nil
+	})
 })
 `
