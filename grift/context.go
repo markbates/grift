8e706d9b1370e074ed48f3e56b5d29c8b01dd635
Mark Bates <mark@markbates.com>
2017-07-08 20:11:10 -0400

    added a few more tests and removed the old Get method

--- grift/context.go
+++ grift/context.go
@@ -2,10 +2,7 @@ package grift
 
 import (
 	"context"
-	"fmt"
-	"log"
-	"path/filepath"
-	"runtime"
+	"sync"
 )
 
 // Context used to pass information between grifts
@@ -15,37 +12,38 @@ type Context struct {
 	Args    []string
 	Verbose bool
 	data    map[interface{}]interface{}
+	moot    *sync.Mutex
 }
 
-// Get a piece of data from the Context.
-func (c *Context) Get(key string) interface{} {
-	warningMsg := "Context#Get is deprecated. Please use Context#Value instead."
-
-	_, file, no, ok := runtime.Caller(1)
-	if ok {
-		file = filepath.Base(file)
-		warningMsg = fmt.Sprintf("Context#Get is deprecated. Please use Context#Value instead. Called from grifts/%s:%d", file, no)
-	}
-	log.Println(warningMsg)
-
-	return c.data[key]
-}
-
+// Value returns a value from the context for the given key
 func (c *Context) Value(key interface{}) interface{} {
-	return c.data[key]
+	if s, ok := key.(string); ok {
+		if v, ok := c.data[s]; ok {
+			return v
+		}
+	}
+	return c.Context.Value(key)
 }
 
 // Set a piece of data onto the Context.
 func (c *Context) Set(key string, val interface{}) {
+	c.moot.Lock()
+	defer c.moot.Unlock()
 	c.data[key] = val
 }
 
 // NewContext builds and returns a new default Context.
 func NewContext(name string) *Context {
+	return NewContextWithContext(name, context.Background())
+}
+
+// NewContextWithContext builds and returns a new default Context given an existing context
+func NewContextWithContext(name string, ctx context.Context) *Context {
 	return &Context{
-		Context: context.Background(),
+		Context: ctx,
 		Name:    name,
 		Args:    []string{},
 		data:    map[interface{}]interface{}{},
+		moot:    &sync.Mutex{},
 	}
 }
