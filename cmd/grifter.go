fce059715dcf5875478d077b0d2d678d1b83ceb0
Eric Ren <mail@ericren.net>
2018-10-26 12:00:26 -0700

    read package path from go.mod if it exists (#30)

--- cmd/grifter.go
+++ cmd/grifter.go
@@ -1,14 +1,16 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/pkg/errors"
+	"github.com/rogpeppe/go-internal/modfile"
 	"html/template"
+	"io/ioutil"
 	"os"
 	"path"
 	"path/filepath"
 	"strings"
 	"sync"
-
-	"github.com/pkg/errors"
 )
 
 const exePath = ".grifter/main.go"
@@ -69,7 +71,24 @@ func newGrifter(name string) (*grifter, error) {
 			return g, errors.Errorf("There is no directory named 'grifts'. Run '%s init' or switch to the appropriate directory", name)
 		}
 		g.GriftsAbsolutePath = filepath.ToSlash(filepath.Join(currentPath, "grifts"))
-		g.GriftsPackagePath = filepath.ToSlash(filepath.Join(path.Base(currentPath), "grifts"))
+
+		// check for go module to see if we can get go.mod
+		if os.Getenv("GO111MODULE") == "on" {
+			moddata, err := ioutil.ReadFile("go.mod")
+			if err != nil {
+				return g, errors.New("go.mod cannot be read or does not exist while go module is enabled.")
+			}
+			packagePath := modfile.ModulePath(moddata)
+			if packagePath == "" {
+				return g, errors.New("go.mod is malformed.")
+			}
+			g.GriftsPackagePath = fmt.Sprintf("%s/grifts", packagePath)
+		} else {
+			// no go module, infer package path from current directory
+			g.GriftsPackagePath = filepath.ToSlash(filepath.Join(path.Base(currentPath), "grifts"))
+		}
+
+
 	}
 
 	return g, nil
@@ -101,3 +120,4 @@ func (g *grifter) Setup() error {
 func (g *grifter) TearDown() error {
 	return os.RemoveAll(filepath.Dir(exePath))
 }
+
